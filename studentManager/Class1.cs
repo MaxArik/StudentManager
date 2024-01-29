c5ecff58e0fd2ada72431edac42035d2fb048b99
MaxArik <149028670+MaxArik@users.noreply.github.com>
2024-01-29 11:02:26 +0800

    Add files via upload

---
+++ studentManager/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentManager
+{
+    internal class Student
+    {
+        public String name;
+        public int age;
+        public String course;
+            
+    }
+
+    public Student(String name, int age, String course)
+    {
+        
+
+    }
+}
